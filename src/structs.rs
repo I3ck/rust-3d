49ccf0d04ae51d25ca45b03783c06cd39a56fec0
Martin Buck <buckmartin@buckmartin.de>
2016-05-05 18:18:35 +0200

    started adding octree

--- src/structs.rs
+++ src/structs.rs
@@ -37,3 +37,23 @@ pub struct KdNode {
     pub val: Point,
     pub dimension: i8
 }
+
+pub struct OctTree {
+    pub root: OctNode,
+    pub min: Point,
+    pub max: Point
+}
+
+pub enum OctNode {
+    Leaf(Point),
+    Internal {
+        tfl: Option<Box<OctNode>>, //top front left
+        tfr: Option<Box<OctNode>>, //top front right
+        tbl: Option<Box<OctNode>>, //top back left
+        tbr: Option<Box<OctNode>>, //top back right
+        bfl: Option<Box<OctNode>>, //bottom front left
+        bfr: Option<Box<OctNode>>, //bottom front right
+        bbl: Option<Box<OctNode>>, //bottom back left
+        bbr: Option<Box<OctNode>>  //bottom back right
+    }
+}
