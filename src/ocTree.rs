f60a90cb75237df27336e86491c5c805b629627a
Martin Buck <buckmartin@buckmartin.de>
2016-05-07 13:59:41 +0200

    implemented constructor of OcTree

--- src/ocTree.rs
+++ src/ocTree.rs
@@ -1,8 +1,21 @@
 use point::{Point};
+use pointCloud::{PointCloud};
 use ocNode::{OcNode};
 
 pub struct OcTree {
     pub root: OcNode,
     pub min: Point,
     pub max: Point
+}
+
+impl OcTree {
+    pub fn new(pc: PointCloud) -> Option<OcTree> {
+        match pc.bbox() {
+            None => None,
+            Some((min, max)) => {
+                let root = OcNode::new(&min, &max, pc.data);
+                Some(OcTree {root: root, min: min, max: max})
+            }
+        }
+    }
 }
