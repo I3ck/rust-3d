e22501afa290b00a145f7b101e9183744d5a308f
Martin Buck <buckmartin@buckmartin.de>
2016-05-24 17:52:29 +0200

    added length dir to plane3d

--- src/plane3D.rs
+++ src/plane3D.rs
@@ -5,6 +5,7 @@ use traits::{IsPlane3D, HasPosition3D};
 pub struct Plane3D<P> where P: HasPosition3D {
     pub origin: P,
     pub normal: P,
+    pub length_dir: P,
     pub width: f64,
     pub height: f64
 }
@@ -15,15 +16,17 @@ impl<P> IsPlane3D<P> for Plane3D<P> where P: HasPosition3D {
         Box::new(Plane3D {
             origin: *P::new(),
             normal: *P::new(),
+            length_dir: *P::new(),
             width: 0.0,
             height: 0.0
         })
     }
 
-    fn build(origin: P, normal: P, width: f64, height: f64) -> Box<Self> {
+    fn build(origin: P, normal: P, length_dir: P, width: f64, height: f64) -> Box<Self> {
         Box::new(Plane3D {
             origin: origin,
             normal: normal,
+            length_dir: length_dir,
             width: width,
             height: height
         })
@@ -43,6 +46,14 @@ impl<P> IsPlane3D<P> for Plane3D<P> where P: HasPosition3D {
 
     fn set_normal(&mut self, normal: P) {
         self.normal = normal
+    }
+
+    fn length_dir(&self) -> P {
+        self.length_dir.clone()
+    }
+
+    fn set_length_dir(&mut self, length_dir: P) {
+        self.length_dir = length_dir
     }
 
     fn width(&self) -> f64 {
