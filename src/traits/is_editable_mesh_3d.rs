5e1488c172a548ecb05fcaabc073df062a13559e
Martin Buck <buckmartin@buckmartin.de>
2016-06-05 16:54:35 +0200

    added traits for mesh and editable mesh

---
+++ src/traits/is_editable_mesh_3d.rs
@@ -0,0 +1,8 @@
+use traits::is_3d::Is3D;
+use traits::is_mesh_3d::IsMesh3D;
+
+pub trait IsEditableMesh3D<P> : IsMesh3D<P> where P: Is3D{
+    fn add_vertex(vertex: P) -> usize;
+    fn add_face(v1: P, v2: P, v3: P) -> usize;
+    fn try_add_connection(vid1: usize, vid2: usize, vid3: usize) -> Option<usize>;
+}
---
+++ src/traits/is_mesh_3d.rs
@@ -0,0 +1,9 @@
+use traits::is_3d::Is3D;
+
+pub trait IsMesh3D<P> where P: Is3D {
+    fn num_faces() -> usize;
+    fn num_vertices() -> usize;
+    fn face_vertex_ids(faceid: usize) -> Option<(usize, usize, usize)>;
+    fn face_vertices(faceid: usize) -> Option<(P, P, P)>;
+    fn vertex(vertexid: usize) -> Option<P>;
+}
