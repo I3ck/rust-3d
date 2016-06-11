3c599542fdd0a9fb64d04923475e078f7baa8878
Martin Buck <buckmartin@buckmartin.de>
2016-06-11 15:24:16 +0200

    finished p3d tests

--- src/lib.rs
+++ src/lib.rs
@@ -164,14 +164,10 @@ pub mod tests {
         assert!(cross23.z() == 1.0 * 2.0 - 0.0 * 2.0);
 
 
-        /*
-        assert!(p2.cross(&p3) == 2.0);
-        assert!(p2.dot(&p3) == 2.0);
-
-        assert!(p2.pos() == (p2.x(), p2.y()));
+        assert!(p2.pos() == (p2.x(), p2.y(), p2.z()));
         let mut p2Clone = p2.clone();
         assert!(p2Clone.pos() == p2.pos());
-        assert!(p2.to_str() == "1 0");
+        assert!(p2.to_str() == "1 0 0");
 
         p2Clone.from(p1.clone());
         assert!(p2Clone.pos() == p1.pos());
@@ -188,52 +184,47 @@ pub mod tests {
                 assert!((1.0 - n.abs()).abs() < eps) ;
                 assert!(n.x() == p3.x() / p3.abs());
                 assert!(n.y() == p3.y() / p3.abs());
+                assert!(n.z() == p3.z() / p3.abs());
             }
         }
 
         p1.set_x(3.0);
         p1.set_y(10.0);
+        p1.set_z(11.0);
         assert!(p1.x() == 3.0);
         assert!(p1.y() == 10.0);
+        assert!(p1.z() == 11.0);
 
-        p1.set_pos(3.0, 11.0);
+        p1.set_pos(3.0, 11.0, 14.0);
         assert!(p1.x() == 3.0);
         assert!(p1.y() == 11.0);
+        assert!(p1.z() == 14.0);
 
-        p2.set_pos(1.0, 2.0);
+        p2.set_pos(1.0, 2.0, 3.0);
         p1.add(&p2);
         assert!(p1.x() == 4.0);
         assert!(p1.y() == 13.0);
+        assert!(p1.z() == 17.0);
 
         p1.substract(&p2);
         assert!(p1.x() == 3.0);
         assert!(p1.y() == 11.0);
+        assert!(p1.z() == 14.0);
 
         p1.scale(2.0);
         assert!(p1.x() == 6.0);
         assert!(p1.y() == 22.0);
+        assert!(p1.z() == 28.0);
 
-        p1.set_pos(1.0, 0.0);
-        p1.rotate(0.0, &origin);
-        assert!(p1.x() == 1.0);
-        assert!(p1.y() == 0.0);
-
-        p1.rotate(2.0 * consts::PI, &origin);
-        assert!((1.0 - p1.x()).abs() < eps);
-        assert!((0.0 - p1.y()).abs() < eps);
-
-
-        p1.rotate(consts::PI, &origin);
-        assert!((-1.0 - p1.x()).abs() < eps);
-        assert!((0.0 - p1.y()).abs() < eps);
-
-        match Point2D::parse("1.3 7.9".to_string()) {
+        match Point3D::parse("1.3 7.9 13.7".to_string()) {
             None => assert!(false),
             Some(bp) => {
                 assert!(bp.x() == 1.3);
                 assert!(bp.y() == 7.9);
+                assert!(bp.z() == 13.7);
             }
         }
-        */
+
+        //@todo missing tests for matrix multiplication
     }
 }
