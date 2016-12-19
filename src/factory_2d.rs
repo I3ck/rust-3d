41379e96cdd69f8d5fa8e00ccff1d10f0395b52f
Martin Buck <buckmartin@buckmartin.de>
2016-12-19 21:10:24 +0100

    added ellipse

--- src/factory_2d.rs
+++ src/factory_2d.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use point_2d::Point2D;
 use point_cloud_2d::PointCloud2D;
 use traits::is_buildable_2d::IsBuildable2D;
@@ -6,7 +8,9 @@ use traits::is_editable_2d::IsEditable2D;
 ///@todo entire file has to be added to tests
 ///@todo add some type level checks like diameter > 0 etc., or return Option types
 ///@todo define trait for pc2d factories, later for 3d as well
-
+///@todo remove center as param and create all around origin
+///@todo correct reserving
+///@todo order parameters (e.g. center and n_points always first)
 
 pub fn origin() -> Box<Point2D> {
     Point2D::build(0.0, 0.0)
@@ -48,6 +52,21 @@ pub fn arc<P>(center: &P, diameter: f64, n_points: usize, radians_start: f64, ra
         let radians = radians_start + (i as f64) * p_dist;
         pc.push(*P::build(center.x() + diameter/2.0 * radians_start.cos(),
                           center.y() + diameter/2.0 * radians_start.sin()));
+    }
+    Box::new(pc)
+}
+
+pub fn ellipse<P>(center: &P, a: f64, b: f64, n_points: usize) -> Box<PointCloud2D<P>> where
+    P: IsEditable2D + IsBuildable2D {
+
+    let mut pc = PointCloud2D::new();
+    let p_dist = PI / (n_points - 1) as f64;
+    let angle: f64 = 0.0; //@todo as parameter? or just drop from formulas?
+
+    for i in 0..n_points {
+        let radians = (i as f64) * p_dist;
+        pc.push(*P::build(center.x() + a * radians.cos() * angle.cos() - b * radians.sin() * angle.sin(),
+                          center.y() + a * radians.cos() * angle.sin() + b * radians.sin() * angle.cos()));
     }
     Box::new(pc)
 }
