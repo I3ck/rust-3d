ac22c6833798336244cfbd16154b6e09f9176d18
Martin Buck <buckmartin@buckmartin.de>
2016-12-19 20:49:51 +0100

    added arc

--- src/factory_2d.rs
+++ src/factory_2d.rs
@@ -5,6 +5,7 @@ use traits::is_editable_2d::IsEditable2D;
 
 ///@todo entire file has to be added to tests
 ///@todo add some type level checks like diameter > 0 etc., or return Option types
+///@todo define trait for pc2d factories, later for 3d as well
 
 
 pub fn origin() -> Box<Point2D> {
@@ -22,7 +23,7 @@ pub fn rectangle<P>(center: &P, width: f64, height: f64) -> Box<PointCloud2D<P>>
     Box::new(pc)
 }
 
-pub fn involut_circle<P>(center: &P, diameter: f64, n_points: usize, radians_start: f64, radians_end: f64) -> Box<PointCloud2D<P>>where
+pub fn involut_circle<P>(center: &P, diameter: f64, n_points: usize, radians_start: f64, radians_end: f64) -> Box<PointCloud2D<P>> where
     P: IsEditable2D + IsBuildable2D {
 
     //@todo reserve
@@ -33,6 +34,20 @@ pub fn involut_circle<P>(center: &P, diameter: f64, n_points: usize, radians_sta
         let current = (i as f64) * p_dist;
         pc.push(*P::build(center.x() + diameter/2.0 * (current.cos() + current * current.sin()),
                           center.y() + diameter/2.0 * (current.sin() - current * current.cos())));
+    }
+    Box::new(pc)
+}
+
+pub fn arc<P>(center: &P, diameter: f64, n_points: usize, radians_start: f64, radians_end: f64) -> Box<PointCloud2D<P>> where
+    P: IsEditable2D + IsBuildable2D {
+
+    let mut pc = PointCloud2D::new();
+    let p_dist = (radians_end - radians_start).abs() / (n_points - 1) as f64;
+
+    for i in 0..n_points {
+        let radians = radians_start + (i as f64) * p_dist;
+        pc.push(*P::build(center.x() + diameter/2.0 * radians_start.cos(),
+                          center.y() + diameter/2.0 * radians_start.sin()));
     }
     Box::new(pc)
 }
