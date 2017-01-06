78ee4b5c47d5231074c12a1c595608f23a731867
Martin Buck <buckmartin@buckmartin.de>
2017-01-06 17:21:37 +0100

    added tests for the circle filter 2d

---
+++ tests/filter_circle_2d_test.rs
@@ -0,0 +1,47 @@
+/*
+Copyright 2017 Martin Buck
+This file is part of rust-3d.
+rust-3d is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+rust-3d is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with rust-3d.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+extern crate rust_3d;
+
+use rust_3d::traits::is_buildable_2d::*;
+use rust_3d::traits::is_filter_2d::*;
+use rust_3d::point_2d::*;
+use rust_3d::positive::*;
+use rust_3d::filter_circle_2d::*;
+
+#[test]
+fn filter_circle_2d_test() {
+    let center = *Point2D::build(10.0, -5.0);
+    let radius = Positive::build(3.0).unwrap();
+    let filter = FilterCircle2D::build(center, radius);
+
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(9.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(8.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(7.0, -5.0)));
+    assert!(!filter.is_allowed(&*Point2D::build(6.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(11.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(12.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(13.0, -5.0)));
+    assert!(!filter.is_allowed(&*Point2D::build(14.0, -5.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -4.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -3.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -2.0)));
+    assert!(!filter.is_allowed(&*Point2D::build(10.0, -1.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -6.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -7.0)));
+    assert!( filter.is_allowed(&*Point2D::build(10.0, -8.0)));
+    assert!(!filter.is_allowed(&*Point2D::build(10.0, -9.0)));
+}
