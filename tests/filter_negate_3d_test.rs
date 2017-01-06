dcb6c263e03c3ea1c5f30f2718209e3576ca667b
Martin Buck <buckmartin@buckmartin.de>
2017-01-06 17:33:19 +0100

    added full test for 2d negate filter and file for the 3d filter

---
+++ tests/filter_negate_3d_test.rs
@@ -0,0 +1,27 @@
+/*
+Copyright 2017 Martin Buck
+This file is part of rust-3d.
+rust-3d is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+rust-3d is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with rust-3d.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+extern crate rust_3d;
+
+use rust_3d::traits::is_buildable_3d::*;
+use rust_3d::traits::is_filter_3d::*;
+use rust_3d::point_3d::*;
+use rust_3d::positive::*;
+use rust_3d::filter_negate_3d::*;
+
+#[test]
+fn filter_negate_3d_test() {
+    //@todo
+}
