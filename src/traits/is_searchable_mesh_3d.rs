55816663dc509f265cf71dad382839bf6e9e1ca8
Martin Buck <buckmartin@buckmartin.de>
2017-07-12 21:14:39 +0200

    added docs for searchable mesh trait

--- src/traits/is_searchable_mesh_3d.rs
+++ src/traits/is_searchable_mesh_3d.rs
@@ -20,23 +20,32 @@ use prelude::*;
 /// IsSearchableMesh3D trait used for meshes in 3D space which have extended search methods
 pub trait IsSearchableMesh3D<P> : IsMesh3D<P> where
     P: IsBuildable3D {
-
-    fn num_edges(&self) -> usize;
-
+    /// Should return the edge ids of the given face. Error if id invalid
     fn edges_of_face(&self, faceid: FId) -> Result<(EId, EId, EId)>;
-
+    /// Should return the edges originating at the given vertex (pointing away / having the vertex as tail). Error if id invalid
     fn edges_originating_from_vertex(&self, vertexid: VId) -> Result<Vec<EId>>;
-    fn edges_ending_at_vertex       (&self, vertexid: VId) -> Result<Vec<EId>>;
-    fn edges_of_vertex              (&self, vertexid: VId) -> Result<Vec<EId>>;
-
+    /// Should return the edges ending at the given vertex (pointing to / having the vertex as head). Error if id invalid
+    fn edges_ending_at_vertex(&self, vertexid: VId) -> Result<Vec<EId>>;
+    /// Should return the edges connecting with the vertex. Error if id invalid
+    fn edges_of_vertex(&self, vertexid: VId) -> Result<Vec<EId>>;
+    /// Should return the vertex id of the edge's tail. Error if id invalid
     fn edge_tail(&self, edgeid: EId) -> Result<VId>;
+    /// Should return the vertex id of the edge's head. Error if id invalid
     fn edge_head(&self, edgeid: EId) -> Result<VId>;
+    /// Should return the edge id of the next edge. Error if id invalid
     fn edge_next(&self, edgeid: EId) -> Result<EId>;
+    /// Should return the edge id of the previous edge. Error if id invalid
     fn edge_prev(&self, edgeid: EId) -> Result<EId>;
+    /// Should return the edge id of the twin edge. Error if id invalid, None if there is none
     fn edge_twin(&self, edgeid: EId) -> Result<Option<EId>>;
+    /// Should return the face id of the edges face. Error if id invalid
     fn edge_face(&self, edgeid: EId) -> Result<FId>;
 
-
+    /// Returns the number of edges within the mesh
+    fn num_edges(&self) -> usize {
+        self.num_faces() * 3
+    }
+    /// Returns faces a vertex is part of. Error if id invalid
     fn faces_of_vertex(&self, vertexid: VId) -> Result<Vec<FId>> {
         let edgeids = self.edges_originating_from_vertex(vertexid)?;
 
@@ -46,7 +55,7 @@ pub trait IsSearchableMesh3D<P> : IsMesh3D<P> where
         }
         Ok(result)
     }
-
+    /// Returns the neighbouring faces of the given face which share the same edges. Error if id invalid
     fn face_edge_neighbours(&self, faceid: FId) -> Result<Vec<FId>> {
         let (e1, e2, e3) = self.edges_of_face(faceid)?;
 
@@ -61,10 +70,9 @@ pub trait IsSearchableMesh3D<P> : IsMesh3D<P> where
             add_twin_face(e2)?;
             add_twin_face(e3)?;
         }
-
         Ok(result)
     }
-
+    /// Returns the neighbouring faces of the given face which share the same vertices. Error if id invalid
     fn face_vertex_neighbours(&self, faceid: FId) -> Result<Vec<FId>> {
         let vids = self.face_vertex_ids(faceid)?;
 
