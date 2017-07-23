47d9e10ad96c60d1e9e82fc3e172e4be8ec69b4e
Martin Buck <buckmartin@buckmartin.de>
2017-07-23 14:30:47 +0200

    FilterBox3D now defined via Box3D

--- src/filters/filter_box_3d.rs
+++ src/filters/filter_box_3d.rs
@@ -15,76 +15,42 @@ along with rust-3d.  If not, see <http://www.gnu.org/licenses/>.
 
 //! FilterBox3D, a box filter within 3D space
 
-use std::cmp::{Eq, Ordering};
-use std::hash::{Hash, Hasher};
-
 use prelude::*;
-use distances_3d::*;
 
-#[derive (Debug, PartialEq, PartialOrd, Default, Clone)]
+#[derive (Debug, PartialEq, PartialOrd, Default, Clone, Eq, Hash, Ord)]
 /// FilterBox3D, a box filter within 3D space
 pub struct FilterBox3D {
-    center: Point3D,
-    size_x: Positive,
-    size_y: Positive,
-    size_z: Positive
-}
-
-impl Eq for FilterBox3D {}
-
-impl Ord for FilterBox3D {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let origin = Point3D::default();
-        match sqr_dist_3d(&origin, &self.center).partial_cmp(&sqr_dist_3d(&origin, &other.center)) {
-            Some(x) => x,
-            None => match self.size_x.partial_cmp(&other.size_x) {
-                Some(x) => x,
-                None => match self.size_y.partial_cmp(&other.size_y) {
-                    Some(x) => x,
-                    None => self.size_z.partial_cmp(&other.size_z).unwrap_or(Ordering::Equal)
-                }
-            }
-        }
-    }
-}
-
-impl Hash for FilterBox3D {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.center.hash(state);
-        (self.size_x.get() as u64).hash(state);
-        (self.size_y.get() as u64).hash(state);
-        (self.size_z.get() as u64).hash(state);
-    }
+    box_3d: Box3D
 }
 
 impl FilterBox3D {
     /// Creates a new FilterBox3D with the given parameters
-    pub fn new(center: Point3D, p_size_x: Positive, p_size_y: Positive, p_size_z: Positive) -> Self {
-        FilterBox3D {center: center, size_x: p_size_x, size_y: p_size_y, size_z: p_size_z}
+    pub fn new(box_3d: Box3D) -> Self {
+        FilterBox3D {box_3d: box_3d}
     }
 }
 
 impl IsND for FilterBox3D {
     fn n_dimensions() -> usize {
-        3
+        Box3D::n_dimensions()
     }
 
     fn get_position(&self, dimension: usize) -> Result<f64> {
-        self.center.get_position(dimension)
+        self.box_3d.get_position(dimension)
     }
 }
 
 impl Is3D for FilterBox3D {
     fn x(&self) -> f64 {
-        self.center.x()
+        self.box_3d.x()
     }
 
     fn y(&self) -> f64 {
-        self.center.y()
+        self.box_3d.y()
     }
 
     fn z(&self) -> f64 {
-        self.center.z()
+        self.box_3d.z()
     }
 }
 
@@ -93,66 +59,51 @@ impl IsBuildableND for FilterBox3D {
         if coords.len() != 3 {
             return Err(ErrorKind::DimensionsDontMatch);
         }
-        Ok(Box::new(FilterBox3D::new(Point3D{x: coords[0], y: coords[1], z: coords[2]}, Positive::one(), Positive::one(), Positive::one())))
+        Ok(Box::new(FilterBox3D::new(*Box3D::new_nd(coords)?)))
     }
 
     fn from_nd<P>(&mut self, other: P) -> Result<()> where
         P: IsBuildableND {
 
-        if P::n_dimensions() != 3 {
-            return Err(ErrorKind::DimensionsDontMatch);
-        }
-
-        self.center.set_x(other.get_position(0)?);
-        self.center.set_y(other.get_position(1)?);
-        self.center.set_z(other.get_position(2)?);
-        Ok(())
+        self.box_3d.from_nd(other)
     }
 }
 
 impl IsBuildable3D for FilterBox3D {
     fn new(x: f64, y: f64, z: f64) -> Box<Self> {
-        Box::new(FilterBox3D::new(Point3D{x: x, y: y, z: z}, Positive::one(), Positive::one(), Positive::one()))
+        Box::new(FilterBox3D::new(*Box3D::new(x, y, z)))
     }
 
     fn from<P>(&mut self, other: P)
         where P: Is3D {
 
-        self.center.from(other)
+        self.box_3d.from(other)
     }
 }
 
 impl IsEditableND for FilterBox3D {
     fn set_position(&mut self, dimension: usize, val: f64) -> Result<()> {
-        match dimension {
-            0 => self.center.set_x(val),
-            1 => self.center.set_y(val),
-            2 => self.center.set_z(val),
-            _ => return Err(ErrorKind::DimensionsDontMatch),
-        }
-        Ok(())
+        self.box_3d.set_position(dimension, val)
     }
 }
 
 impl IsEditable3D for FilterBox3D {
     fn set_x(&mut self, val: f64) {
-        self.center.set_x(val);
+        self.box_3d.set_x(val);
     }
 
     fn set_y(&mut self, val: f64) {
-        self.center.set_y(val);
+        self.box_3d.set_y(val);
     }
 
     fn set_z(&mut self, val: f64) {
-        self.center.set_z(val);
+        self.box_3d.set_z(val);
     }
 }
 
 impl HasBoundingBox3D for FilterBox3D {
     fn bounding_box(&self) -> Result<BoundingBox3D> {
-        let p_min = Point3D{x: self.center.x() - self.size_x.get() / 2.0, y: self.center.y() - self.size_y.get() / 2.0, z: self.center.z() - self.size_z.get() / 2.0};
-        let p_max = Point3D{x: self.center.x() + self.size_x.get() / 2.0, y: self.center.y() + self.size_y.get() / 2.0, z: self.center.z() + self.size_z.get() / 2.0};
-        BoundingBox3D::new(&p_min, &p_max)
+        self.box_3d.bounding_box()
     }
 }
 
@@ -160,17 +111,17 @@ impl<T> IsFilter<T> for FilterBox3D where
     T: Is3D {
 
     fn is_allowed(&self, p: &T) -> bool {
-           p.x() >= self.center.x() - self.size_x.get() / 2.0
-        && p.x() <= self.center.x() + self.size_x.get() / 2.0
-        && p.y() >= self.center.y() - self.size_y.get() / 2.0
-        && p.y() <= self.center.y() + self.size_y.get() / 2.0
-        && p.z() >= self.center.z() - self.size_z.get() / 2.0
-        && p.z() <= self.center.z() + self.size_z.get() / 2.0
+           p.x() >= self.box_3d.x() - self.box_3d.size_x.get() / 2.0
+        && p.x() <= self.box_3d.x() + self.box_3d.size_x.get() / 2.0
+        && p.y() >= self.box_3d.y() - self.box_3d.size_y.get() / 2.0
+        && p.y() <= self.box_3d.y() + self.box_3d.size_y.get() / 2.0
+        && p.z() >= self.box_3d.z() - self.box_3d.size_z.get() / 2.0
+        && p.z() <= self.box_3d.z() + self.box_3d.size_z.get() / 2.0
     }
 }
 
 impl From<BoundingBox3D> for FilterBox3D {
     fn from(x: BoundingBox3D) -> Self {
-        Self::new(x.center_bb(), x.size_x(), x.size_y(), x.size_z())
+        Self::new(Box3D{center: x.center_bb(), size_x: x.size_x(), size_y: x.size_y(), size_z: x.size_z()})
     }
 }
--- src/shapes/shapes_3d.rs
+++ src/shapes/shapes_3d.rs
@@ -15,7 +15,10 @@ along with rust-3d.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Containing basic 3D shape data types used by other types / algorithms
 
+use std::cmp::{Eq, Ordering};
+
 use prelude::*;
+use distances_3d::*;
 
 #[derive (Debug, PartialEq, PartialOrd, Clone, Hash, Default)]
 /// Sphere, a sphere in 3D space
@@ -24,6 +27,17 @@ pub struct Sphere {
     pub radius: Positive
 }
 impl Eq for Sphere {}
+
+
+
+
+
+
+
+
+
+
+
 
 #[derive (Debug, PartialEq, PartialOrd, Clone, Hash, Default)]
 /// Box3D, a box in 3D space
@@ -34,3 +48,118 @@ pub struct Box3D {
     pub size_z: Positive
 }
 impl Eq for Box3D {}
+
+
+impl Ord for Box3D {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let origin = Point3D::default();
+        match sqr_dist_3d(&origin, &self.center).partial_cmp(&sqr_dist_3d(&origin, &other.center)) {
+            Some(x) => x,
+            None => match self.size_x.partial_cmp(&other.size_x) {
+                Some(x) => x,
+                None => match self.size_y.partial_cmp(&other.size_y) {
+                    Some(x) => x,
+                    None => self.size_z.partial_cmp(&other.size_z).unwrap_or(Ordering::Equal)
+                }
+            }
+        }
+    }
+}
+
+impl IsND for Box3D {
+    fn n_dimensions() -> usize {
+        3
+    }
+
+    fn get_position(&self, dimension: usize) -> Result<f64> {
+        self.center.get_position(dimension)
+    }
+}
+
+impl Is3D for Box3D {
+    fn x(&self) -> f64 {
+        self.center.x()
+    }
+
+    fn y(&self) -> f64 {
+        self.center.y()
+    }
+
+    fn z(&self) -> f64 {
+        self.center.z()
+    }
+}
+
+impl IsBuildableND for Box3D {
+    fn new_nd(coords: &Vec<f64>) -> Result<Box<Self>> {
+        if coords.len() != 3 {
+            return Err(ErrorKind::DimensionsDontMatch);
+        }
+        Ok(Box::new(Box3D{center: Point3D{x: coords[0], y: coords[1], z: coords[2]}, size_x: Positive::one(), size_y: Positive::one(), size_z: Positive::one()}))
+    }
+
+    fn from_nd<P>(&mut self, other: P) -> Result<()> where
+        P: IsBuildableND {
+
+        if P::n_dimensions() != 3 {
+            return Err(ErrorKind::DimensionsDontMatch);
+        }
+
+        self.center.set_x(other.get_position(0)?);
+        self.center.set_y(other.get_position(1)?);
+        self.center.set_z(other.get_position(2)?);
+        Ok(())
+    }
+}
+
+impl IsBuildable3D for Box3D {
+    fn new(x: f64, y: f64, z: f64) -> Box<Self> {
+        Box::new(Box3D{center: Point3D{x: x, y: y, z: z}, size_x: Positive::one(), size_y: Positive::one(), size_z: Positive::one()})
+    }
+
+    fn from<P>(&mut self, other: P)
+        where P: Is3D {
+
+        self.center.from(other)
+    }
+}
+
+impl IsEditableND for Box3D {
+    fn set_position(&mut self, dimension: usize, val: f64) -> Result<()> {
+        match dimension {
+            0 => self.center.set_x(val),
+            1 => self.center.set_y(val),
+            2 => self.center.set_z(val),
+            _ => return Err(ErrorKind::DimensionsDontMatch),
+        }
+        Ok(())
+    }
+}
+
+impl IsEditable3D for Box3D {
+    fn set_x(&mut self, val: f64) {
+        self.center.set_x(val);
+    }
+
+    fn set_y(&mut self, val: f64) {
+        self.center.set_y(val);
+    }
+
+    fn set_z(&mut self, val: f64) {
+        self.center.set_z(val);
+    }
+}
+
+impl HasBoundingBox3D for Box3D {
+    fn bounding_box(&self) -> Result<BoundingBox3D> {
+        let p_min = Point3D{x: self.center.x() - self.size_x.get() / 2.0, y: self.center.y() - self.size_y.get() / 2.0, z: self.center.z() - self.size_z.get() / 2.0};
+        let p_max = Point3D{x: self.center.x() + self.size_x.get() / 2.0, y: self.center.y() + self.size_y.get() / 2.0, z: self.center.z() + self.size_z.get() / 2.0};
+        BoundingBox3D::new(&p_min, &p_max)
+    }
+}
+
+impl From<BoundingBox3D> for Box3D {
+    fn from(x: BoundingBox3D) -> Self {
+        Box3D{center: x.center_bb(), size_x: x.size_x(), size_y: x.size_y(), size_z: x.size_z()}
+    }
+}
