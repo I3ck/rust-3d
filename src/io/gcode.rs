215ac22870b2961b28d04e3d27fad5317945c509
Martin Buck <buckmartin@buckmartin.de>
2020-03-21 16:35:00 +0100

    Add relative/absolute support to gcode loader

--- src/io/gcode.rs
+++ src/io/gcode.rs
@@ -33,6 +33,7 @@ use std::{
 //------------------------------------------------------------------------------
 
 //@todo this could be extended to consider way more commands besides G1
+//@todo code duplication
 
 /// Loads a IsPushable<Is3D> as x y z coordinates from gcode
 pub fn load_gcode_points<IP, P, R>(read: &mut R, ip: &mut IP) -> GcodeResult<()>
@@ -46,6 +47,7 @@ where
     let mut i_line = 0;
 
     let mut start_pushed = false;
+    let mut ra = RelativeAbsolute::Absolute;
     let mut x = 0.0;
     let mut y = 0.0;
     let mut z = 0.0;
@@ -59,9 +61,49 @@ where
         let line = line_buffer.trim_end();
         i_line += 1;
 
-        if line.starts_with("G1 ") || line.starts_with("G92 ") {
-            //@todo more specific error
+        if line.starts_with("G1 ") {
             let mut any_changed = false;
+            //@todo more specific errors
+            let [opt_x, opt_y, opt_z] = g1_command(line).ok_or(GcodeError::LineParse(i_line))?;
+
+            if let Some(new_x) = opt_x {
+                any_changed = true;
+                match ra {
+                    RelativeAbsolute::Absolute => x = new_x,
+                    RelativeAbsolute::Relative => x += new_x,
+                }
+            }
+
+            if let Some(new_y) = opt_y {
+                any_changed = true;
+                match ra {
+                    RelativeAbsolute::Absolute => y = new_y,
+                    RelativeAbsolute::Relative => y += new_y,
+                }
+            }
+
+            if let Some(new_z) = opt_z {
+                any_changed = true;
+                match ra {
+                    RelativeAbsolute::Absolute => z = new_z,
+                    RelativeAbsolute::Relative => z += new_z,
+                }
+            }
+
+            if any_changed {
+                if !start_pushed {
+                    ip.push(P::new(0.0, 0.0, 0.0));
+                    start_pushed = true
+                }
+                ip.push(P::new(x, y, z));
+            }
+        } else if line.starts_with("G90 ") {
+            ra = RelativeAbsolute::Absolute;
+        } else if line.starts_with("G91 ") {
+            ra = RelativeAbsolute::Relative;
+        } else if line.starts_with("G92 ") {
+            let mut any_changed = false;
+            //@todo more specific error
             let [opt_x, opt_y, opt_z] = g1_command(line).ok_or(GcodeError::LineParse(i_line))?;
 
             if let Some(new_x) = opt_x {
@@ -122,6 +164,11 @@ fn skip_first_char(text: &str) -> &str {
     let mut cs = text.chars();
     cs.next();
     cs.as_str()
+}
+
+enum RelativeAbsolute {
+    Relative,
+    Absolute,
 }
 
 /// Error type for .gcode file operations
