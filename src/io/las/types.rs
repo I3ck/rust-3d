b54cde343969058ea10721782e071e9ad76f4236
Martin Buck <buckmartin@buckmartin.de>
2020-04-12 11:17:10 +0200

    Add point data formats for las

--- src/io/las/types.rs
+++ src/io/las/types.rs
@@ -28,7 +28,7 @@ use super::super::from_bytes::FromBytesError;
 
 //------------------------------------------------------------------------------
 
-#[derive (Debug)]
+#[derive(Debug)]
 pub struct Header {
     //COUNT_BYTES_THIS COUNT_BYTES_TOTAL_HERE
     pub file_signature: [u8; 4],       //4 4 //@todo 'signed' char in spec
@@ -72,8 +72,100 @@ pub struct Header {
 
 //------------------------------------------------------------------------------
 
+pub struct PointData {
+    pub x: i32, //4 4
+    pub y: i32, //4 8
+    pub z: i32, //4 12
+}
+
+pub struct ColorData {
+    pub red: u16,   //2 2
+    pub green: u16, //2 4
+    pub blue: u16,  //2 6
+}
+
+pub struct WaveData {
+    pub wave_descriptor_index: u8,      //1 1
+    pub offset_waveform_data: u64,      //8 9
+    pub waveform_packet_size: u32,      //4 13
+    pub return_point_waveform_loc: f32, //4 17
+    pub dx: f32,                        //4 21
+    pub dy: f32,                        //4 25
+    pub dz: f32,                        //4 29
+}
+
+pub struct Format0 {
+    pub point_data: PointData, //12 12
+    pub intensity: u16,        //2 14
+    pub bitdata: u8, //1 15 ReturnNumber 3 bits, NumberOfReturns 3 bits, ScanDirFlag 1 bit, EdgeOfFlightLine 1 bit
+    pub classification: u8, //1 16
+    pub scan_angle_rank: u8, //1 17 actually signed char
+    pub user_data: u8, // 1 18
+    pub point_source_id: u16, //2 20
+}
+
+pub struct Format1 {
+    pub format_0: Format0, //20 20
+    pub gps_time: f64,     //8 28
+}
+
+pub struct Format2 {
+    pub format_0: Format0,     //20 20
+    pub color_data: ColorData, //6 26
+}
+
+pub struct Format3 {
+    pub format_1: Format1,     //28 28
+    pub color_data: ColorData, //6 34
+}
+
+pub struct Format4 {
+    pub format_1: Format1,   //28 28
+    pub wave_data: WaveData, //29 57
+}
+
+pub struct Format5 {
+    pub format_3: Format3,   //34 34
+    pub wave_data: WaveData, //29 63
+}
+
+pub struct Format6 {
+    pub point_data: PointData, //12 12
+    pub intensity: u16,        //2 14
+    pub bitdata: u16, //2 16 //Return Number 4 bits, NumberofReturns 4bits, ClassificationFlags 4 bits, ScannerChannel 2 bits, ScanDirFlag: 1bit, EdgeOfFlightLine 1bit
+    pub classification: u8, //1 17
+    pub user_data: u8, //1 18
+    pub scan_angle: i16, //2 20
+    pub point_source_id: u16, //2 22
+    pub gps_time: f64, //8 30
+}
+
+pub struct Format7 {
+    pub format_6: Format6,     //30 30
+    pub color_data: ColorData, //6 36
+}
+
+pub struct Format8 {
+    pub format_7: Format7, //36 36
+    pub nir: u16,          //2 38
+}
+
+pub struct Format9 {
+    pub format_6: Format6,   //30 30
+    pub wave_data: WaveData, //29 59
+}
+
+pub struct Format10 {
+    pub format_6: Format6,     //30 30
+    pub color_data: ColorData, //6 36
+    pub nir: u16,              //2 38
+    pub wave_data: WaveData,   //29 67
+}
+
+//------------------------------------------------------------------------------
+
 /// Error type for .las file operation
-#[derive (Debug)]
+#[derive(Debug)]
 pub enum LasError {
     AccessFile,
     BinaryData,
