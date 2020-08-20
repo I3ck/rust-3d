0e29ceb1172b8af7ad1efc992ed76e524178dc01
Martin Buck <buckmartin@buckmartin.de>
2020-08-20 15:09:13 +0200

    Implement psl iterator

--- src/io/psl.rs
+++ src/io/psl.rs
@@ -27,9 +27,123 @@ use crate::*;
 use std::{
     fmt,
     io::{Error as ioError, Read},
+    iter::FusedIterator,
+    marker::PhantomData,
 };
 
 use super::byte_reader::*;
+
+//------------------------------------------------------------------------------
+
+pub struct PslIterator<P, R>
+where
+    P: IsBuildable3D,
+    R: Read,
+{
+    read: R,
+    is_done: bool,
+    is_initialized: bool,
+    n_passes_left: i32,
+    n_lines_left: i32,
+    n_point_left: i32,
+    phantom: PhantomData<P>,
+}
+
+impl<P, R> PslIterator<P, R>
+where
+    P: IsBuildable3D,
+    R: Read,
+{
+    pub fn new(read: R) -> Self {
+        Self {
+            read,
+            is_done: false,
+            is_initialized: false,
+            n_passes_left: 0,
+            n_lines_left: 0,
+            n_point_left: 0, //@todo pointS
+            phantom: PhantomData,
+        }
+    }
+    #[inline(always)]
+    fn initialize(&mut self) -> PslResult<()> {
+        self.n_passes_left = fetch_header_return_n_passes(&mut self.read)?;
+        Ok(())
+    }
+    #[inline(always)]
+    fn fetch_counts(&mut self) -> PslResult<()> {
+        self.n_lines_left = if self.n_lines_left == 0 && self.n_passes_left > 0 {
+            fetch_pass_header_return_n_lines(&mut self.read)?
+        } else {
+            0
+        };
+        self.n_point_left = if self.n_point_left == 0 && self.n_lines_left > 0 {
+            fetch_line_header_return_n_points(&mut self.read)?
+        } else {
+            0
+        };
+        Ok(())
+    }
+    #[inline(always)]
+    fn reduce_count(&mut self) {
+        if self.n_point_left > 0 {
+            self.n_point_left -= 1
+        } else {
+            if self.n_lines_left > 0 {
+                self.n_lines_left -= 1
+            } else {
+                if self.n_passes_left > 0 {
+                    self.n_passes_left -= 1
+                }
+            }
+        }
+    }
+}
+
+impl<P, R> Iterator for PslIterator<P, R>
+where
+    P: IsBuildable3D,
+    R: Read,
+{
+    type Item = PslResult<P>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_done {
+            return None;
+        }
+
+        if !self.is_initialized {
+            self.is_initialized = true;
+            if let Err(e) = self.initialize() {
+                self.is_done = true;
+                return Some(Err(e));
+            }
+        }
+
+        if let Err(e) = self.fetch_counts() {
+            self.is_done = true;
+            return Some(Err(e));
+        }
+
+        if self.n_point_left == 0 {
+            self.is_done = true;
+            return None;
+        }
+
+        self.reduce_count();
+
+        return Some(fetch_point(&mut self.read).map_err(|e| {
+            self.is_done = true;
+            e
+        }));
+    }
+}
+
+impl<P, R> FusedIterator for PslIterator<P, R>
+where
+    P: IsBuildable3D,
+    R: Read,
+{
+}
 
 //------------------------------------------------------------------------------
 
@@ -40,18 +154,10 @@ where
     P: IsBuildable3D,
     R: Read,
 {
-    let n_passes = fetch_header_return_n_passes(read)?;
+    let iterator = PslIterator::new(read);
 
-    for _ in 0..n_passes {
-        let n_lines = fetch_pass_header_return_n_lines(read)?;
-
-        for _ in 0..n_lines {
-            let n_points = fetch_line_header_return_n_points(read)?;
-
-            for _ in 0..n_points {
-                ip.push(fetch_point(read)?);
-            }
-        }
+    for p in iterator {
+        ip.push(p?)
     }
 
     Ok(())
